c6a1a406c83fe85e1f792531fba7dd380c94a6c1
dm.choi <dm.choi@logiall.com>
2023-07-22 10:39:12 +0900

    layout: masterlayout _app.tsx로 분리

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import '~/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '~/styles/globals.css';
+import type { AppProps } from 'next/app';
+import MasterLayout from '~/components/layout/MasterLayout';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <MasterLayout>
+      <Component {...pageProps} />
+    </MasterLayout>
+  );
 }
