9c1aa20b4ea40cf286424e98db811034eb623f2e
dmchoi77 <dm.choi.dev@gmail.com>
2023-08-09 00:15:02 +0900

    feat: Portal 추가

--- src/components/dialog/Dialog.tsx
+++ src/components/dialog/Dialog.tsx
@@ -1,5 +1,6 @@
 import { forwardRef } from 'react';
 import Button from '../button/Button';
+import { GlobalPortal } from '~/GlobalPortal';
 
 interface IProps {
   onClose: () => void;
@@ -11,71 +12,73 @@ interface IProps {
 const Dialog = forwardRef<HTMLDivElement, IProps>(
   ({ onClose, mainText, cancelText, handleConfirm, handleCancel }, ref) => {
     return (
-      <div
-        ref={ref}
-        css={{
-          width: '100%',
-          height: '100%',
-          padding: '20px',
-          position: 'absolute',
-          top: 0,
-          backgroundColor: '#0000007a',
-          display: 'flex',
-          margin: '0 auto',
-          zIndex: 999,
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
+      <GlobalPortal.Consumer>
         <div
+          ref={ref}
           css={{
-            width: '300px',
-            height: '300px',
-            backgroundColor: '#ffffff',
-            borderRadius: '15px',
-            padding: '13px',
-            boxSizing: 'border-box',
+            width: '100%',
+            height: '100%',
+            padding: '20px',
+            position: 'absolute',
+            top: 0,
+            backgroundColor: '#0000007a',
+            display: 'flex',
+            margin: '0 auto',
+            zIndex: 999,
+            justifyContent: 'center',
+            alignItems: 'center',
           }}
         >
           <div
             css={{
-              display: 'flex',
-              justifyContent: 'space-between',
-            }}
-          >
-            <div css={{ fontWeight: 600 }}>알림</div>
-            <div onClick={onClose} css={{ cursor: 'pointer', fontWeight: 600 }}>
-              X
-            </div>
-          </div>
-          <div
-            css={{
-              display: 'flex',
-              flexDirection: 'column',
-              height: '100%',
+              width: '300px',
+              height: '300px',
+              backgroundColor: '#ffffff',
+              borderRadius: '15px',
+              padding: '13px',
+              boxSizing: 'border-box',
             }}
           >
             <div
               css={{
-                height: '220px',
-                paddingTop: '10px',
-                paddingBottom: '10px',
+                display: 'flex',
+                justifyContent: 'space-between',
               }}
             >
-              {mainText ?? '모달입니다 모달'}
+              <div css={{ fontWeight: 600 }}>알림</div>
+              <div onClick={onClose} css={{ cursor: 'pointer', fontWeight: 600 }}>
+                X
+              </div>
             </div>
             <div
               css={{
                 display: 'flex',
-                gap: '10px',
+                flexDirection: 'column',
+                height: '100%',
               }}
             >
-              {cancelText && <Button text={cancelText} onClick={handleCancel ?? onClose} />}
-              <Button text='확인' onClick={handleConfirm} />
+              <div
+                css={{
+                  height: '220px',
+                  paddingTop: '10px',
+                  paddingBottom: '10px',
+                }}
+              >
+                {mainText ?? '모달입니다 모달'}
+              </div>
+              <div
+                css={{
+                  display: 'flex',
+                  gap: '10px',
+                }}
+              >
+                {cancelText && <Button text={cancelText} onClick={handleCancel ?? onClose} />}
+                <Button text='확인' onClick={handleConfirm} />
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </GlobalPortal.Consumer>
     );
   },
 );
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,15 +3,18 @@ import '~/styles/globals.css';
 import type { AppProps } from 'next/app';
 
 import MasterLayout from '~/components/layout/MasterLayout';
+import { GlobalPortal } from '~/GlobalPortal';
 
 // import { AppProvider } from '~/store/AppContext';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     // <AppProvider>
-    <MasterLayout>
-      <Component {...pageProps} />
-    </MasterLayout>
+    <GlobalPortal.Provider>
+      <MasterLayout>
+        <Component {...pageProps} />
+      </MasterLayout>
+    </GlobalPortal.Provider>
     // </AppProvider>
   );
 }
