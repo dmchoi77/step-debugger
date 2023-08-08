cc04053202235a8718d88c8270df98f788319861
dm.choi <dm.choi@logiall.com>
2023-08-08 15:28:03 +0900

    fix: 다이얼로그 onClose 추가, style 작업

--- src/components/dialog/Dialog.tsx
+++ src/components/dialog/Dialog.tsx
@@ -1,13 +1,17 @@
 import { forwardRef } from 'react';
+import Button from '../button/Button';
 
-const Dialog = forwardRef<HTMLDivElement, any>((props, ref) => {
+interface IProps {
+  onClose: () => void;
+}
+const Dialog = forwardRef<HTMLDivElement, IProps>(({ onClose }, ref) => {
   return (
     <div
       ref={ref}
       css={{
         width: '100%',
         height: '100%',
-
+        padding: '20px',
         position: 'absolute',
         top: 0,
         backgroundColor: '#0000007a',
@@ -24,10 +28,39 @@ const Dialog = forwardRef<HTMLDivElement, any>((props, ref) => {
           height: '300px',
           backgroundColor: '#ffffff',
           borderRadius: '15px',
-          padding: '15px',
+          padding: '13px',
+          boxSizing: 'border-box',
         }}
       >
-        <div>모달입니다 모달</div>
+        <div
+          css={{
+            display: 'flex',
+            justifyContent: 'space-between',
+          }}
+        >
+          <div css={{ fontWeight: 600 }}>알림</div>
+          <div onClick={onClose} css={{ cursor: 'pointer', fontWeight: 600 }}>
+            X
+          </div>
+        </div>
+        <div
+          css={{
+            display: 'flex',
+            flexDirection: 'column',
+            height: '100%',
+          }}
+        >
+          <div
+            css={{
+              height: '220px',
+              paddingTop: '10px',
+              paddingBottom: '10px',
+            }}
+          >
+            모달입니다 모달
+          </div>
+          <Button text='확인' onClick={onClose} />
+        </div>
       </div>
     </div>
   );
