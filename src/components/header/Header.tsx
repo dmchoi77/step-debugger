5f2ce77f0158c99aff96e0750dbbf138ef3139be
dm.choi <dm.choi@logiall.com>
2023-08-08 18:07:35 +0900

    fix(Header): SVG, router.back 추가

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,4 +1,6 @@
 import styled from '@emotion/styled';
+import { LeftChevron } from '../svgs';
+import { useRouter } from 'next/router';
 
 const HeaderContainer = styled.header`
   width: 100%;
@@ -7,13 +9,15 @@ const HeaderContainer = styled.header`
 
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: flex-start;
 `;
 
 const Header: React.FC = () => {
+  const router = useRouter();
   return (
     <HeaderContainer>
-      <span>F12 개발자 도구를 열어보세요</span>
+      <LeftChevron css={{ cursor: 'pointer', margin: '10px' }} onClick={() => router.back()} />
+      {/* <span>F12 개발자 도구를 열어보세요</span> */}
     </HeaderContainer>
   );
 };
